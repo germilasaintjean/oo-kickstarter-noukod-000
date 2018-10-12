9d90d97933ada4a72ac50ed4a74d593394557866
Saint-Jean Germila <germila@noukod.com>
2018-10-12 18:56:48 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -2,18 +2,15 @@ class Backer
 
   attr_accessor  :name, :backed_projects, :backers
 
-#@@all = []
-
   def initialize(name)
-   @name = name
-  @backed_projects = []
-  @backers = []
+     @name = name
+     @backed_projects = []
+     @backers = []
   end
-
-
   def back_project(project)
     self.backed_projects << project
     end
-
-
+ def back_project(backer)
+   self.backed_projects << backer
+ end
 end
