4728cf66392dd31e844fa14261045b92f10a0e4e
Saint-Jean Germila <germila@noukod.com>
2018-10-12 18:28:49 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -6,11 +6,14 @@ class Backer
 
   def initialize(name)
    @name = name
-  @backed_projects = []
+  @backed_project = []
   end
 
 def self.backed_projects
   @@backed_projects
   end
-
+  
+  def self.backed_project(project)
+    self.backed_projects.save
+    end
 end
