c69caf2dfcb6b3be0826c77345e5228d4f8d7148
Saint-Jean Germila <germila@noukod.com>
2018-10-12 18:15:53 +0000

    automatically backed up by learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,9 +1,9 @@
 class Projects
 
-  attr_accessor :name, :backers
+  attr_accessor :title, :backers
 
-  def initialize(name)
-   @name = name
+  def initialize(title)
+   @title = title
    @backers = []
   end
 end
